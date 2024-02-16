3139f869582ce67504b8dbcffa5e00a9854ba7df
Curtttt <95266553+Curtttt@users.noreply.github.com>
2024-02-16 22:43:57 +0700

    Delete my-web directory

--- my-web/src/environments/environment.ts
+++
@@ -1,9 +0,0 @@
-export const firebaseConfig = {
-    apiKey: "AIzaSyDNTX77oV6FaXShPz_h74o1VBqsv7KO5AI",
-    authDomain: "badminton-f9a3f.firebaseapp.com",
-    projectId: "badminton-f9a3f",
-    storageBucket: "badminton-f9a3f.appspot.com",
-    messagingSenderId: "620862202534",
-    appId: "1:620862202534:web:13a049c00e68b54bf70a67",
-    measurementId: "G-JD7RJLHY0L"
-};
